3f80e437ea6d65fe3a87bb3c149701b413e503f0
Ademar Alves de Oliveira <ademar.oliveira@wizeline.com>
2017-12-07 16:19:58 -0600

    Using cmake

--- Unix/build.sh
+++ Unix/build.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-mkdir -p cache
+mkdir -p build
+cd build
+cmake ..
+make
 
-if [ -f "cache/sqlite.o" ]
-then
-  echo "Using cached sqlite3"
-else
-  echo "Compiling sqlite3"
-  gcc -c -o cache/sqlite.o ../Core/sqlite/sqlite3.c
-fi
-
-echo "Compiling main"
-g++ -c -o cache/main.o main.cpp -I ../Core -I ../Core/sqlite
-
-echo "Building executable"
-g++ -o main cache/sqlite.o cache/main.o
-
-echo "Done, execute with ./main"
+echo "Done, execute with ./build/main"
